4ee03f580f5f5083987f80dc84657d81a803dc79
Phelipe Tozzi <phelipetozzi@hotmail.com>
2020-05-22 15:46:15 -0300

    Atributos e métodos adicionados

--- Personagem.cs
+++ Personagem.cs
@@ -2,6 +2,16 @@ namespace PrimeiroPoo
 {
     public class Personagem
     {
-        
+        public string nome = "Tony Stark";
+
+        public int idade = 35;
+
+        public string armadura;
+
+
+        public string Atacar(){
+            return "O personagem atacou!";
+        }
+
     }
 }
